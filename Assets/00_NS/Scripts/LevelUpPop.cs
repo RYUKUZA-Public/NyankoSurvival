872569c6b8ab3c532b2b77ddb714d5c1fe88e644
RYU <hakrimryu@gmail.com>
2023-04-16 18:29:35 +0900

    [InGame] Level Upポップアップ時、Itemを3つ、ランダム抽選する機能作成

--- Assets/00_NS/Scripts/LevelUpPop.cs
+++ Assets/00_NS/Scripts/LevelUpPop.cs
@@ -13,6 +13,7 @@ public class LevelUpPop : MonoBehaviour
 
     public void Show()
     {
+        GetRandomitems();
         _rect.localScale = Vector3.one;
         GameManager.Instance.TimeStop();
     }
@@ -26,5 +27,36 @@ public class LevelUpPop : MonoBehaviour
     public void Select(int index)
     {
         _items[index].OnClick();
+    }
+    
+    private void GetRandomitems()
+    {
+        // すべてのアイテムを非活性
+        foreach (Item item in _items)
+            item.gameObject.SetActive(false);
+        
+        // ランダムアイテム3つ抽選
+        int[] ran = new int[3];
+        while (true)
+        {
+            ran[0] = Random.Range(0, _items.Length);
+            ran[1] = Random.Range(0, _items.Length);
+            ran[2] = Random.Range(0, _items.Length);
+            
+            if(ran[0] != ran[1] && ran[1] != ran[2] && ran[0] != ran[2])
+                break;
+        }
+
+        for (int i = 0; i < ran.Length; i++)
+        {
+            Item ranItem = _items[ran[i]];
+
+            // Maxレベルの場合は消費アイテムで対処
+            if (ranItem.level == ranItem.data.damages.Length)
+                _items[4].gameObject.SetActive(true);
+            else
+                ranItem.gameObject.SetActive(true);
+        }
+
     }
 }
