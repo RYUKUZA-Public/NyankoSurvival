f41fc5e0a6cb664e7f37e9a8d02a8f083c9ec128
RYU <hakrimryu@gmail.com>
2023-04-17 01:33:18 +0900

    [Fix] TileとEnemyを再配置する際、PlayerのInput値ではなく、位置の違いだけで、再配置するように修正

--- Assets/00_NS/Scripts/Reposition.cs
+++ Assets/00_NS/Scripts/Reposition.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -27,36 +26,35 @@ public class Reposition : MonoBehaviour
         // 現在のタイルマップの位置
         Vector3 myPos = transform.position;
         // Playerの入力方向
-        Vector3 playerDir = GameManager.Instance.Player.InputVec;
-        
-        // PlayerとタイルマップのX、Y軸距離差計算
-        float diffX = playerTransform.position.x - myPos.x;
-        float diffY = playerTransform.position.y - myPos.y;
+        //Vector3 playerDir = GameManager.Instance.Player.InputVec;
         
         switch (transform.tag)
         {
             // タイルマップのタグがTileの場合
             case "Tile":
+                // PlayerとタイルマップのX、Y軸距離差計算
+                float diffX = playerTransform.position.x - myPos.x;
+                float diffY = playerTransform.position.y - myPos.y;
+                float dirX = diffX < 0 ? -1 : 1;
+                float dirY = diffY < 0 ? -1 : 1;
+                diffX = Mathf.Abs(diffX);
+                diffY = Mathf.Abs(diffY);
+                
                 // X、 Y軸距離差の中で大きいものを基準にタイルマップを移動
-                if (Mathf.Abs(diffX) > Mathf.Abs(diffY))
-                {
-                    // タイルマップ サイズ 40
-                    // diffXの符号に従ってタイルマップを右または左に移動
-                    transform.Translate(Vector3.right * Mathf.Sign(diffX) * 40);
-                    
-                }
-                else if (Mathf.Abs(diffX) < Mathf.Abs(diffY))
-                {
-                    // diffYの符号に従ってタイルマップを上または下に移動
-                    transform.Translate(Vector3.up * Mathf.Sign(diffY) * 40);
-                }
+                // タイルマップ サイズ 40
+                // diffXの符号に従ってタイルマップを右または左に移動
+                if (diffX > diffY)
+                    transform.Translate(Vector3.right * dirX * 40);
+                // diffYの符号に従ってタイルマップを上または下に移動
+                else if (diffX < diffY)
+                    transform.Translate(Vector3.up * dirY * 40);
                 break;
             case "Enemy":
-                // TODO. Enemyが生きている時だけ
                 if (_coll.enabled)
                 {
-                    transform.Translate(playerDir * 20 + new Vector3
-                        (Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0));
+                    Vector3 dist = playerTransform.position - myPos;
+                    Vector3 ran = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+                    transform.Translate(ran + dist * 2);
                 }
                 break;
         }
