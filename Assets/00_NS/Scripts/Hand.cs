bb97bb1e894e5dc2193219268373056740f27f0c
RYU <hakrimryu@gmail.com>
2023-04-16 16:04:11 +0900

    [BugFix] Playerが、Flip状態の場合、武器を装着する際に、武器の位置が適用されないバグ修正

--- Assets/00_NS/Scripts/Hand.cs
+++ Assets/00_NS/Scripts/Hand.cs
@@ -7,24 +7,27 @@ public class Hand : MonoBehaviour
     
     private Vector3 _localPos;
     private Quaternion _localRot;
-    private bool _previousIsReverse;
 
-    private void Awake()
+    private void OnEnable()
     {
         _player = transform.parent.GetComponent<SpriteRenderer>();
         _localPos = transform.localPosition;
         _localRot = transform.localRotation;
-        _previousIsReverse = _player.flipX;
+        UpdateHand();
     }
-
+    
     private void LateUpdate()
     {
+        UpdateHand();
+    }
+    
+    private void UpdateHand()
+    {
         bool isReverse = _player.flipX;
-        if (isReverse != _previousIsReverse) {
+        if (sprite.flipX != isReverse) {
+            sprite.flipX = isReverse;
             transform.localPosition = isReverse ? new Vector3(-_localPos.x, _localPos.y, _localPos.z) : _localPos;
             transform.localRotation = isReverse ? Quaternion.Euler(_localRot.eulerAngles.x, _localRot.eulerAngles.y, -_localRot.eulerAngles.z) : _localRot;
-            sprite.flipX = isReverse;
-            _previousIsReverse = isReverse;
         }
     }
 }
