fd6ce16546051365685d53d57e53eff5510ec433
RYU <hakrimryu@gmail.com>
2023-04-17 23:26:57 +0900

    [Fix] 既存、switchを使用していた、HUDをDictionaryに変更して、性能改善

--- Assets/00_NS/Scripts/UI/HUD.cs
+++ Assets/00_NS/Scripts/UI/HUD.cs
@@ -1,48 +1,61 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HUD : NewMonoBehaviour
 {
-    public enum InfoType { Exp, Level, Kill, Time, Hp }
     [SerializeField]
-    private InfoType type;
+    private GameDefine.PlayerInfoType type;
 
-    private Text myText;
-    private Slider mySlider;
-
+    private Text _myText;
+    private Slider _mySlider;
+    private GameManager _gameManager;
+    
+    private Dictionary<GameDefine.PlayerInfoType, System.Action> _updateMethods = 
+        new Dictionary<GameDefine.PlayerInfoType, System.Action>();
+    
     private void Awake()
     {
-        myText = GetComponent<Text>();
-        mySlider = GetComponent<Slider>();
+        _myText = GetComponent<Text>();
+        _mySlider = GetComponent<Slider>();
+        _gameManager = GameManager.Instance;
+        
+        // 各タイプに合わせた、アップデート関数をディクショナリーに登録
+        _updateMethods[GameDefine.PlayerInfoType.Exp] = UpdateExp;
+        _updateMethods[GameDefine.PlayerInfoType.Level] = UpdateLevel;
+        _updateMethods[GameDefine.PlayerInfoType.Kill] = UpdateKill;
+        _updateMethods[GameDefine.PlayerInfoType.Time] = UpdateTime;
+        _updateMethods[GameDefine.PlayerInfoType.Hp] = UpdateHp;
     }
 
-    public override void NewLateUpdate()
+    /// <summary>
+    /// ディクショナリーから、タイプに合わせたアップデート関数呼び出し
+    /// </summary>
+    public override void NewLateUpdate() => _updateMethods[type]?.Invoke();
+
+    private void UpdateExp()
     {
-        switch (type)
-        {
-            case InfoType.Exp:
-                float currentExp = GameManager.Instance.Exp;
-                float maxExp = GameManager.Instance.NextExp[Mathf.Min(GameManager.Instance.Level, GameManager.Instance.NextExp.Length - 1)];
-                mySlider.value = currentExp / maxExp;
-                break;
-            case InfoType.Level:
-                myText.text = $"Lv.{GameManager.Instance.Level:F0}";
-                break;
-            case InfoType.Kill:
-                myText.text = $"{GameManager.Instance.Kill:F0}";
-                break;
-            case InfoType.Time:
-                float remainTime = GameManager.Instance.MaxGameTime - GameManager.Instance.GameTime;
-                int min = Mathf.FloorToInt(remainTime / 60); 
-                int sec = Mathf.FloorToInt(remainTime % 60);
-                myText.text = $"{min:D2}:{sec:D2}";
-                break;
-            case InfoType.Hp:
-                float currentHp = GameManager.Instance.Hp;
-                float maxHp = GameManager.Instance.MaxHp;
-                mySlider.value = currentHp / maxHp;
-                break;
-        }
+        float currentExp = _gameManager.Exp;
+        float maxExp = _gameManager.NextExp[Mathf.Min(_gameManager.Level, _gameManager.NextExp.Length - 1)];
+        _mySlider.value = currentExp / maxExp;
+    }
+    
+    private void UpdateLevel() => _myText.text = $"Lv.{_gameManager.Level:F0}";
+
+    private void UpdateKill() => _myText.text = $"{_gameManager.Kill:F0}";
+
+    private void UpdateTime()
+    {
+        float remainTime = _gameManager.MaxGameTime - _gameManager.GameTime;
+        int min = Mathf.FloorToInt(remainTime / 60); 
+        int sec = Mathf.FloorToInt(remainTime % 60);
+        _myText.text = $"{min:D2}:{sec:D2}";
+    }
+    
+    private void UpdateHp()
+    {
+        float currentHp = _gameManager.Hp;
+        float maxHp = _gameManager.MaxHp;
+        _mySlider.value = currentHp / maxHp;
     }
 }
